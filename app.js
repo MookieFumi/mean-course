f8b1b677621b6f2c19601448526245b185775be9
MookieFumi <mookiefumi@outlook.com>
2015-03-25 21:20:17 +0100

    Extend routes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// set DEBUG=mean-course & nodemon .\bin\www
+// set DEBUG=mean-course & nodemon app.js
 
 var express = require('express');
 var path = require('path');
@@ -6,10 +6,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var debug = require('debug')('mean-course:server');
 var http = require('http');
+var routes = require('./routes/index');
+
 var app = express();
 
 // view engine setup
@@ -26,7 +26,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -75,10 +74,6 @@ app.set('port', port);
  */
 
 var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
 
 server.listen(port);
 server.on('error', onError);
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//     res.render('index', {
+//         title: 'Express'
+//     });
+// });
+
+router.get('/help', function(req, res, next) {
+    res.render('help', {
+        title: 'Help page',
+        message: 'Help page by MookieFumi!'
+    });
+});
+
+router.get('/help/:content', function(req, res, next) {
+    res.render('help', {
+        title: 'Help page',
+        message: 'Help page by MookieFumi!',
+        content: req.params.content
+    });
 });
 
 module.exports = router;
